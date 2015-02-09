7f79f89a565b1669a4666ebbd7f5cd61785b10d7
dozencrows <git@magwitch.uk.net>
2015-02-09 21:46:08 +0000

    Fixup makefile

--- src/common/rules.mk
+++ src/common/rules.mk
@@ -4,7 +4,7 @@
 TTY ?= /dev/ttyUSB*
 
 # use the arm cross compiler, not std gcc
-TRGT = arm-none-eabi-
+TRGT = /home/ntuckett/NXP-LPC/gcc-arm-none-eabi-4_9-2014q4/bin/arm-none-eabi-
 CC = $(TRGT)gcc
 CXX = $(TRGT)g++
 CP = $(TRGT)objcopy
@@ -12,15 +12,16 @@ CP = $(TRGT)objcopy
 # compiler and linker settings
 CFLAGS = -mcpu=cortex-m0plus -mthumb -I../common -Os -ggdb
 CXXFLAGS = $(CFLAGS) -fno-rtti -fno-exceptions
-LDFLAGS = -Wl,--script=../common/LPC810.ld -nostartfiles
+LDFLAGS = -Wl,--script=../common/LPC810.ld -nostartfiles -Wl,-Map=firmware.map
 
 # permit including this makefile from a sibling directory
 vpath %.c ../common
 vpath %.cpp ../common
 
 # default target
-upload: firmware.bin
-	lpc21isp -control -donotstart -bin $< $(TTY) 115200 0
+#upload: firmware.bin
+#	lpc21isp -control -donotstart -bin $< $(TTY) 115200 0
+all: firmware.bin
 
 firmware.elf: main.o gcc_startup_lpc8xx.o
 	$(CC) -o $@ $(CFLAGS) $(LDFLAGS) $^
@@ -32,4 +33,4 @@ clean:
 	rm -f *.o *.elf
 
 # these target names don't represent real files
-.PHONY: upload clean
+.PHONY: clean
