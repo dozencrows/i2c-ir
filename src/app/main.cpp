8704f77becd7443154080529dbc077242f7c479f
dozencrows <git@magwitch.uk.net>
2015-02-11 22:24:33 +0000

    Added SCT-driven square wave

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -45,7 +45,52 @@ void initMainClock() {
     while (!(LPC_SYSCON->MAINCLKUEN & 0x01));
 }
 
-void i2cSetupRecv (), i2cSetupSend (int); // forward
+#define SW_FREQ                 38000
+#define SW_MATCH_PERIOD         (FIXED_CLOCK_RATE_HZ / SW_FREQ)
+#define SW_MATCH_HALF_PERIOD    (SW_MATCH_PERIOD / 2)
+
+void initSCTSquareWave() {
+    // Turn on the clock to the SCT
+    LPC_SYSCON->SYSAHBCLKCTRL |= (1 << 8);
+    
+    // Set up output pin - (ISP)
+    LPC_SWM->PINASSIGN6 = 0x01FFFFFF;
+
+    LPC_SCT->CONFIG = 0;        // 16 bit counter timers
+    
+    // Set up two match events for halfway through and at the end
+    LPC_SCT->REGMODE_L      = 0;
+    LPC_SCT->MATCH[0].L     = SW_MATCH_PERIOD;
+    LPC_SCT->MATCHREL[0].L  = SW_MATCH_PERIOD;
+    LPC_SCT->MATCH[1].L     = SW_MATCH_HALF_PERIOD;
+    LPC_SCT->MATCHREL[1].L  = SW_MATCH_HALF_PERIOD;
+    
+    // Toggle output pin on events to generate square wave
+    LPC_SCT->OUT[0].SET     = 0x01;
+    LPC_SCT->OUT[0].CLR     = 0x02;
+    
+    // Set up event control to simply enable events 1 and 2,
+    // and associate them with L counter, match registers 0 and 1.
+    LPC_SCT->EVENT[0].CTRL  = 0x5000;
+    LPC_SCT->EVENT[0].STATE = 0x01;
+    LPC_SCT->EVENT[1].CTRL  = 0x5001;
+    LPC_SCT->EVENT[1].STATE = 0x01;
+    
+    // Set up counter limit on event 1 to restart
+    LPC_SCT->LIMIT_L        = 0x01;    
+}
+
+void sctSquareWaveOn() {
+    LPC_SCT->CTRL_L &= ~(1<<2);
+}
+
+void sctSquareWaveOff() {
+    LPC_SCT->CTRL_L |= (1<<2);  // Stop
+    LPC_SCT->CTRL_L |= (1<<3);  // Clear counter
+    LPC_SCT->OUTPUT = 0;        // Clear output
+}
+
+void i2cSetupRecv (), i2cSetupSend (int);
 
 void i2cSetup () {
     for (int i = 0; i < 3000000; ++i) __ASM("");
@@ -65,19 +110,16 @@ extern "C" void I2C0_IRQHandler () {
     LPC_I2CD_API->i2c_isr_handler(ih);
 }
 
-// called when I2C reception has been completed
 void i2cRecvDone (uint32_t err, uint32_t) {
     i2cSetupRecv();
     if (err == 0)
         i2cSetupSend(i2cRecvBuf[1]);
 }
 
-// called when I2C transmission has been completed
 void i2cSendDone (uint32_t err, uint32_t) {
     i2cSetupRecv();
 }
 
-// prepare to receive the register number
 void i2cSetupRecv () {
     i2cParam.func_pt = i2cRecvDone;
     i2cParam.num_bytes_send = 0;
@@ -86,16 +128,31 @@ void i2cSetupRecv () {
     LPC_I2CD_API->i2c_slave_receive_intr(ih, &i2cParam, &i2cResult);
 }
 
-// prepare to transmit either the byte count or the actual data
 void i2cSetupSend (int regNum) {
     i2cParam.func_pt = i2cSendDone;
     i2cParam.num_bytes_rec = 0;
-    if (regNum == 0) {
-        i2cParam.num_bytes_send = 1;
-        i2cSendBuf[0] = 0xff;
-    } else {
-        i2cParam.num_bytes_send = 16;
-        strcpy((char*)i2cSendBuf, "0123456789ABCDEF");
+    switch (regNum) {
+        case 0:
+            i2cParam.num_bytes_send = 1;
+            i2cSendBuf[0] = 0xff;
+            break;
+            
+        case 1:
+            i2cParam.num_bytes_send = 16;
+            strcpy((char*)i2cSendBuf, "0123456789ABCDEF");
+            break;
+
+        case 2:
+            i2cParam.num_bytes_send = 1;
+            i2cSendBuf[0] = 0xfe;
+            sctSquareWaveOn();
+            break;
+
+        case 3:
+            i2cParam.num_bytes_send = 1;
+            i2cSendBuf[0] = 0xfd;
+            sctSquareWaveOff();
+            break;
     }
     i2cParam.buffer_ptr_send = i2cSendBuf;
     LPC_I2CD_API->i2c_slave_transmit_intr(ih, &i2cParam, &i2cResult);
@@ -108,7 +165,7 @@ int main () {
     puts("i2c-ir started");
     i2cSetup();
     i2cSetupRecv();
-
+    initSCTSquareWave();
     puts("Waiting...");
     while (true) {
         __WFI();
