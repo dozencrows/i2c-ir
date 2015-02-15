b454304ae9d0b49e5ab14d6e7d6b990eeaab66bf
dozencrows <git@magwitch.uk.net>
2015-02-15 17:04:34 +0000

    Fixed ms delay timer; implemented repeat sends with inter-delay

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -27,7 +27,7 @@ void timersInit() {
 }
 
 void delayMs(int milliseconds) {
-    LPC_MRT->Channel[2].INTVAL = (((FIXED_CLOCK_RATE_HZ / 250L) * milliseconds) >> 2) - 286;
+    LPC_MRT->Channel[2].INTVAL = (FIXED_CLOCK_RATE_HZ / 1000L) * milliseconds;
 
     while (LPC_MRT->Channel[2].STAT & 0x02)
         ; //wait while running
@@ -56,22 +56,33 @@ void initMainClock() {
 #define SW_MODULATE_PERIOD_B_OFF  2000
 
 // Times are in tenths of uS
-uint16_t g_testWaveForm[] = { 24000, 6000, 12000, 6000, 6000, 6000, 0, 0 };
-volatile int g_nextCycle = 0;
+uint16_t g_testWaveForm[]   = { 24000, 6000, 12000, 6000, 6000, 6000, 0, 0 };
+volatile int g_nextCycle    = 0;
+volatile int g_waveDone     = 0;
+int g_repeatCount           = 0;
+int g_repeatDelayMs         = 2; 
 
 extern "C" void SCT_IRQHandler(void) {
-    if (g_testWaveForm[g_nextCycle]) {
-        LPC_SCT->MATCHREL[2].H  = g_testWaveForm[g_nextCycle];
-        LPC_SCT->MATCHREL[0].H  = g_testWaveForm[g_nextCycle++] + g_testWaveForm[g_nextCycle++];
-    }
-    else {
-        // To finish, event 0 set to halt both timers and clear output
-        LPC_SCT->HALT_L     = 0x01;
-        LPC_SCT->HALT_H     = 0x01;
-        LPC_SCT->OUT[0].CLR = 0x01;
-    }
+    uint32_t events = LPC_SCT->EVFLAG;
     
-    LPC_SCT->EVFLAG |= 0xf;
+    if (events & 0x08) {
+        if (g_testWaveForm[g_nextCycle]) {
+            LPC_SCT->MATCHREL[2].H  = g_testWaveForm[g_nextCycle];
+            LPC_SCT->MATCHREL[0].H  = g_testWaveForm[g_nextCycle++] + g_testWaveForm[g_nextCycle++];
+        }
+        else {
+            // To finish, event 0 set to halt both timers, clear output and fire final event
+            LPC_SCT->HALT_L     = 0x01;
+            LPC_SCT->HALT_H     = 0x01;
+            LPC_SCT->OUT[0].CLR = 0x01;
+            LPC_SCT->EVEN       = 0x01;
+        }
+    }
+    else if (events & 0x01) {
+        g_waveDone = 1;
+    }
+        
+    LPC_SCT->EVFLAG = events;
 }
 
 void initSCTSquareWave() {
@@ -80,6 +91,8 @@ void initSCTSquareWave() {
     // -- Common timer initialisation --
     
     LPC_SYSCON->SYSAHBCLKCTRL |= (1 << 8);  // Turn on the clock to the SCT
+    LPC_SYSCON->PRESETCTRL &= ~(1<<8);      // reset SCT
+    LPC_SYSCON->PRESETCTRL |=  (1<<8);
     LPC_SWM->PINASSIGN6 = 0x01FFFFFF;       // Set up output pin - (ISP)
     LPC_SCT->CONFIG = (1<<17)|(1<<18);      // H and L 16 bit counter timers, using match reg 0 each to limit
 
@@ -119,12 +132,11 @@ void initSCTSquareWave() {
     LPC_SCT->EVENT[3].CTRL  = 0x5012;
     LPC_SCT->EVENT[3].STATE = 0x01;
     LPC_SCT->STOP_L         = 0x08;
-    LPC_SCT->EVEN           = 0x08;
 
     NVIC_EnableIRQ(SCT_IRQn);
 }
 
-void sctSquareWaveOn() {
+void sctSendSignal(int repeats) {
     LPC_SCT->HALT_L     = 0x00;
     LPC_SCT->HALT_H     = 0x00;
     LPC_SCT->OUT[0].CLR = 0x0a;     // events 1 and 3 clear output
@@ -140,14 +152,38 @@ void sctSquareWaveOn() {
     LPC_SCT->MATCH[2].H  = g_testWaveForm[g_nextCycle];
     LPC_SCT->MATCH[0].H  = g_testWaveForm[g_nextCycle++] + g_testWaveForm[g_nextCycle++];
     
+    LPC_SCT->EVEN = 0x08;
+    g_waveDone = 0;
     LPC_SCT->CTRL_U |= (1<<3)|(1<<19);  // Clear counters
+    
+    if (repeats > 0) {
+        g_repeatCount = repeats;
+    }
+    
     LPC_SCT->CTRL_H &= ~(1<<2);   
 }
 
-void sctSquareWaveOff() {
+void sctStopSignal() {
     LPC_SCT->CTRL_U |= (1<<2)|(1<<18);  // Halt counters
     LPC_SCT->CTRL_U |= (1<<3)|(1<<19);  // Clear counters
     LPC_SCT->OUTPUT = 0;                // Clear output
+    g_repeatCount = 0;
+    g_waveDone = 0;
+}
+
+void sctUpdateSignal() {
+    if (g_waveDone) {
+        g_repeatCount--;
+        if (g_repeatCount) {
+            delayMs(g_repeatDelayMs);
+            //puts("Repeat...");
+            sctSendSignal(0);
+        }
+        else {
+            puts("Done.");
+            g_waveDone = 0;
+        }
+    }
 }
 
 void i2cSetupRecv (), i2cSetupSend (int);
@@ -205,13 +241,13 @@ void i2cSetupSend (int regNum) {
         case 2:
             i2cParam.num_bytes_send = 1;
             i2cSendBuf[0] = 0xfe;
-            sctSquareWaveOn();
+            sctSendSignal(3);
             break;
 
         case 3:
             i2cParam.num_bytes_send = 1;
             i2cSendBuf[0] = 0xfd;
-            sctSquareWaveOff();
+            sctStopSignal();
             break;
     }
     i2cParam.buffer_ptr_send = i2cSendBuf;
@@ -220,6 +256,7 @@ void i2cSetupSend (int regNum) {
 
 int main () {
     initMainClock();
+    timersInit();
     serial.init(LPC_USART0, FIXED_UART_BAUD_RATE);
     delayMs(100);
     puts("i2c-ir started");
@@ -229,6 +266,7 @@ int main () {
     puts("Waiting...");
     while (true) {
         __WFI();
+        sctUpdateSignal();
     }
 }
 
