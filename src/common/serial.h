6384050c252595133ae30f12373a2999b23ab789
dozencrows <git@magwitch.uk.net>
2015-04-11 14:48:04 +0100

    Fixed UART stoppage on reset

--- src/common/serial.h
+++ src/common/serial.h
@@ -6,8 +6,6 @@ public:
     void init (UART* u, uint32_t baudRate) {
         uart = u;
 
-        //const uint32_t uartclkdiv = 1;
-        //const uint32_t SystemCoreClock = 12000000;
         const uint32_t uartclkdiv = 5;                  // Values for 10Mhz system clock from PLL
         const uint32_t SystemCoreClock = 60000000;
 
@@ -16,6 +14,7 @@ public:
         LPC_SYSCON->SYSAHBCLKCTRL |= (1<<14);   // enable uart0 clock
         LPC_SYSCON->PRESETCTRL &= ~(1<<3);      // reset uart0
         LPC_SYSCON->PRESETCTRL |= (1<<3);       // release uart0
+        LPC_SWM->PINASSIGN0 = 0xffff0004;
 
         // configure UART
         uint32_t clk = SystemCoreClock / uartclkdiv;
@@ -24,6 +23,9 @@ public:
         LPC_SYSCON->UARTFRGDIV = 0xFF;
         LPC_SYSCON->UARTFRGMULT = (((clk/16) * (LPC_SYSCON->UARTFRGDIV+1)) /
                 (baudRate * (uart->BRG+1))) - (LPC_SYSCON->UARTFRGDIV+1);
+        //LPC_USART0->BRG = 5;
+        //LPC_SYSCON->UARTFRGDIV = 0xff;
+        //LPC_SYSCON->UARTFRGMULT = 21;
 
         uart->STAT = (1<<5) | (1<<11);          // clear status bits
         uart->CFG |= 1<<0;                      // enable uart
