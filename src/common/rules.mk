0c4e1c54d0f34d89545e79c8c40746e88c4867fe
dozencrows <github@magwitch.uk.net>
2015-05-03 21:53:59 +0100

    Made makefile rules transportable

--- src/common/rules.mk
+++ src/common/rules.mk
@@ -4,7 +4,7 @@
 TTY ?= /dev/ttyUSB*
 
 # use the arm cross compiler, not std gcc
-TRGT = /home/ntuckett/NXP-LPC/gcc-arm-none-eabi-4_9-2014q4/bin/arm-none-eabi-
+TRGT = ../../../gcc-arm-none-eabi-4_9-2014q4/bin/arm-none-eabi-
 CC = $(TRGT)gcc
 CXX = $(TRGT)g++
 CP = $(TRGT)objcopy
